ad6fb9bcf00801a8b78f1ec0aff9c21f03908a2e
bishalrauniyar <bishalrauniyar64@gmail.com>
2026-03-25 02:11:13 +0545

    add unit tests with pytest

---
+++ test_app.py
@@ -0,0 +1,23 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_status_code(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_home_message(client):
+    response = client.get("/")
+    data = response.get_json()
+    assert data["message"] == "Hello Genese Solution! My CI/CD task is running!"
+
+def test_status_check(client):
+    response = client.get("/status")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["status"] == "ok"
